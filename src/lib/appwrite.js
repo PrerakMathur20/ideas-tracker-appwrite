25b7fd60948b303b7a72930b45d4c6c78ab445c6
Prerak Mathur <mathur.prerak@gmail.com>
2023-10-21 04:54:30 +0530

    Tutorial steps

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,7 +3,7 @@ import { Client, Databases, Account } from "appwrite";
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("<YOUR_PROJECT_ID>"); // Replace with your project ID
+  .setProject("<653307c1a2aeaaf13773>");  // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
